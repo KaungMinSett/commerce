a228e33483144afdd70ccdcbd9a96f5ea788bbb1
Kaung Min Sat <kaungminsat@Kaungs-MacBook-Pro.local>
2024-04-17 16:20:07 +0630

    4 necessary models created

---
+++ auctions/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Auction_lists(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=1000)
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField()
+    Category = models.ManyToManyField(Category, related_name="auctions")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="auctions")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+class Bids(models.Model):
+    auction = models.ForeignKey(Auction_lists, on_delete=models.CASCADE, related_name="bids")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    bid = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comments(models.Model):
+    auction = models.ForeignKey(Auction_lists, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    comment = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
