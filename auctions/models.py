6e73cf4804d11daa63c1d86e1eac34ccdf6a1a5d
Kaung Min Sat <kaungminsat@Kaungs-MacBook-Pro.local>
2024-04-29 17:56:16 +0630

    added string representations for models

--- auctions/models.py
+++ auctions/models.py
@@ -23,6 +23,9 @@ class Auction_lists(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
 
+    def __str__(self):
+        return f" {self.title} at {self.starting_bid} by {self.created_by}"
+
  
 
 class Bids(models.Model):
@@ -31,13 +34,22 @@ class Bids(models.Model):
     bid = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.bid} by {self.user} on {self.auction}"
+
 class Comments(models.Model):
     auction = models.ForeignKey(Auction_lists, on_delete=models.CASCADE, related_name="commenters")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     comment = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.comment} by {self.user} on {self.auction}"
+
 class Watchlists(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watchlists")
     auction = models.ForeignKey(Auction_lists, on_delete=models.CASCADE, related_name="watchedby")
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} watching {self.auction}"
